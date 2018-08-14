b37b4b44566edc2855d52b4af552fcf7666dd981
amir9979 <amir9979@gmail.com>
2018-08-14 15:21:16 +0300

    mvn run

--- run_mvn.py
+++ run_mvn.py
@@ -7,22 +7,35 @@ import csv
 csv.field_size_limit(sys.maxsize)
 
 SURFIRE_DIR_NAME = 'surefire-reports'
+OBSERVE_PATH = r"c:\temp\observe"
+
+class Test(object):
+    def __init__(self, junit_test):
+        self.junit_test = junit_test
+        self.classname = junit_test.classname
+        self.name = junit_test.name
+        result = 'pass'
+        if type(junit_test.result) is Error:
+            result = 'error'
+        if type(junit_test.result) is Failure:
+            result = 'failure'
+        self.out_come = result
+
+    def __repr__(self):
+        return "{classname}@{name}: {outcome}".format(classname=self.classname, name=self.name, outcome=self.outcome)
+
 
 def get_tests_results(repo_dir):
     surefire_reports = get_surefire_files(repo_dir)
-    outcomes = {}
+    outcomes = []
     for report in surefire_reports:
         try:
             for case in JUnitXml.fromfile(report):
-                result = 'pass'
-                if type(case.result) is Error:
-                    result = 'error'
-                if type(case.result) is Failure:
-                    result = 'failure'
-                outcomes["{classname}@{name}".format(classname=case.classname, name=case.name)] = result
+                outcomes.append(Test(case))
         except:
             pass
     return outcomes
+
 
 def get_surefire_files(repo_dir):
     surefire_files = []
@@ -32,20 +45,42 @@ def get_surefire_files(repo_dir):
                 surefire_files.append(os.path.join(root, name))
     return surefire_files
 
+
 def run_mvn_on_commits(commits, git_path):
     for commit in commits:
-        git_commit_path = r'{0}_{1}'.format(git_path, commit)
-        Popen(['git', 'clone', git_path, git_commit_path]).communicate()
-        Popen(['git', 'checkout', '-f', '{0}~1'.format(commit)], cwd=git_commit_path).communicate()
-        os.system(r'mvn install -fn  -f {0}'.format(git_commit_path))
-        res = get_tests_results(git_commit_path)
-        failed_tests = map(lambda x: x[0], filter(lambda x: x[1] != 'pass' and x[1] != 'error', res.items()))
-        with open(r'C:\Temp\example\failed_tests_{0}.txt'.format(commit), 'wb') as f:
-            f.writelines(failed_tests)
+        observe_tests(commit, git_path)
+
+
+def observe_tests(commit_to_observe, git_path):
+    git_commit_path = checkout_commit(commit_to_observe, git_path)
+    add_java_agent_tracer(git_commit_path)
+    run_mvn(git_commit_path)
+    return get_tests_results(git_commit_path)
+
+
+def add_java_agent_tracer(git_commit_path):
+    poms = []
+    for root, _, files in os.walk(git_commit_path):
+        poms.extend(map(lambda name: os.path.join(root, name), filter(lambda name: name == "pom.xml", files)))
+    map(fix_pom_file, poms)
+
+
+def fix_pom_file(pom_path):
+    pass
+
+
+def run_mvn(git_commit_path):
+    os.system(r'mvn install -fn  -f {0}'.format(git_commit_path))
+
+
+def checkout_commit(commit_to_observe, git_path):
+    git_commit_path = os.path.join(OBSERVE_PATH, os.path.basename(git_path), commit_to_observe)
+    Popen(['git', 'clone', git_path, git_commit_path]).communicate()
+    Popen(['git', 'checkout', '-f', '{0}'.format(commit_to_observe)], cwd=git_commit_path).communicate()
+    return git_commit_path
 
 
 if __name__ == "__main__":
-    import fixing_issues
     commits_file = r"C:\Temp\commits3.csv"
     git_path = r"C:\Temp\example\airavata"
     fixing_issues.main(commits_file, git_path, r"http://issues.apache.org/jira", r"AIRAVATA")
