2cd0e9da936b8bf92ca28ade39661cabf056f9c7
Amir Elmishali <amir9979@gmail.com>
2020-04-13 15:54:48 +0300

    Update repo.py

--- repo.py
+++ repo.py
@@ -4,14 +4,16 @@ from subprocess import Popen
 
 
 class Repo(object):
-    REPO_DIR = r"C:\Temp\apache_repos"
-    # REPO_DIR = r"Z:\ev_repos"
+    #REPO_DIR = r"C:\Temp\apache_repos"
+    REPO_DIR = r"Z:\ev_repos"
 
-    def __init__(self, jira_key, github_name):
+    def __init__(self, jira_key, github_name, local_path=None):
         self.jira_key = jira_key
         self.github_name = github_name
-        self.local_path = os.path.join(Repo.REPO_DIR, jira_key)
-
+        if local_path:
+            self.local_path = local_path
+        else:
+            self.local_path = os.path.join(Repo.REPO_DIR, jira_key)
         self.clone_if_needed()
 
     def clone_if_needed(self):
