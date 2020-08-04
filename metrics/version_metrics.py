9f74456da272a26619e96b0a903f9a02d2486957
amir9979 <amir9979@gmail.com>
2020-08-05 00:42:21 +0300

    Update version_metrics.py

--- metrics/version_metrics.py
+++ metrics/version_metrics.py
@@ -348,7 +348,7 @@ class SourceMonitor(Extractor):
     def _process_metrics(self, out_dir):
         files_path = os.path.join(out_dir, "source_monitor_classes.csv")
         files_df = pd.read_csv(files_path, encoding = "ISO-8859-8", error_bad_lines=False)
-        files_df["Maximum Block Depth"] = files_df["Maximum Block Depth"].apply(lambda x: float(x.replace('+')))
+        files_df["Maximum Block Depth"] = files_df["Maximum Block Depth"].apply(lambda x: float(x.replace('+', '')))
         cols_to_drop = ["Project Name", "Checkpoint Name", "Created On"]
         for i in cols_to_drop + ['Name of Most Complex Method*']:
             files_df = files_df.drop(i, axis=1)
