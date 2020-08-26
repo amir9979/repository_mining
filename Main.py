e606dc693d0bf55bf86e1bdeb8668b60dc03db8c
amir9979 <amir9979@gmail.com>
2020-08-26 21:26:16 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -60,10 +60,14 @@ class Main():
         methods_instance.predict()
 
     def aggrate_methods_df(self, df):
+        def clean(s):
+            if "@" in s:
+                return s[1].split('@')[1].split('.')[:-1][-1]
+            return s[1].split('.')[:-1][-1]
         ids = df['Method_ids'].iteritems()
         files_id, classes_id = tee(ids, 2)
         files = pd.Series(list(map(lambda x: x[1].split('@')[0], files_id))).values
-        classes = pd.Series(list(map(lambda x: x[1].split('.')[:-1][-1], classes_id))).values
+        classes = pd.Series(list(map(clean, classes_id))).values
         df.insert(0, 'File', files)
         df.insert(0, 'Class', classes)
         groupby = ['File', 'Class']
