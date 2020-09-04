30eaeb39654ac8d537e22520dfee6dbf880191cc
inbalros <inbalros@post.bgu.ac.il>
2020-09-04 04:55:44 +0300

    deal with empty selection_list

--- version_selector.py
+++ version_selector.py
@@ -140,7 +140,9 @@ class BinSelectVersion(AbstractSelectVersions):
                 else:
                     configuration = {'start': start, 'step': step, 'stop': stop, 'versions': selected_versions}
                     self.selected_versions.append(configuration)
-        return list(map(lambda x: x['versions'], self.selected_versions))[self.selected_config]
+        if len(self.selected_versions) == 0:
+			return None
+		return list(map(lambda x: x['versions'], self.selected_versions))[self.selected_config]
 
     def _store_versions(self, repo):
         config = Config().config
