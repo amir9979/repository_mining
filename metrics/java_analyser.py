b39a7ca7a8d8656a9513eb4e997f958be767e609
amir9979 <amir9979@gmail.com>
2020-11-01 16:15:07 +0200

    designite

--- metrics/java_analyser.py
+++ metrics/java_analyser.py
@@ -72,7 +72,6 @@ class JavaParserFileAnalyser(FileAnalyser):
         df = self.parser_df.loc[cond]
         closest_df = df.iloc[(df["Method Beginning Line"] - line).abs().argsort()[:1]]
         if closest_df.empty:
-            print("get_closest_id", file_name, relative_file_name, self.parser_df['File Path'].to_list())
             return None
         file_path = str(closest_df["File Path"].values[0])
         package_name = str(closest_df["Package Name"].values[0])
