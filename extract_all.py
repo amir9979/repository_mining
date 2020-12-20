1c993938d7a8e911b84007161903505bb18cfb7f
amir9979 <amir9979@gmail.com>
2020-12-20 03:04:13 +0200

    Update extract_all.py

--- extract_all.py
+++ extract_all.py
@@ -7,6 +7,8 @@ def extract_all():
     for n in map(lambda x: x.name, ProjectName):
         m = Main()
         m.set_project_enum(n)
+        m.choose_versions(version_num=3, algorithm='bin',
+                             version_type=VersionType["Untyped"], strict=False)
         m.set_version_selection(version_num=3, algorithm='bin',
                                    version_type=VersionType["Untyped"], strict=False,
                                 selected_config=0)
