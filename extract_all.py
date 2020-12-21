27ee628673d483cbfa2d312af5e47cff87a0681b
amir9979 <amir9979@gmail.com>
2020-12-21 15:39:16 +0200

    Update extract_all.py

--- extract_all.py
+++ extract_all.py
@@ -1,19 +1,19 @@
 from Main import Main
 from projects import ProjectName
 from version_selector import VersionType
+import sys
 
 
-def extract_all():
-    for n in map(lambda x: x.name, ProjectName):
-        m = Main()
-        m.set_project_enum(n)
-        m.choose_versions(version_num=3, algorithm='bin',
-                             version_type=VersionType["Untyped"], strict=False)
-        m.set_version_selection(version_num=3, algorithm='bin',
-                                   version_type=VersionType["Untyped"], strict=False,
-                                selected_config=0)
-        m.extract()
+def extract_all(name):
+    m = Main()
+    m.set_project_enum(name)
+    m.choose_versions(version_num=3, algorithm='bin',
+                         version_type=VersionType["Untyped"], strict=False)
+    m.set_version_selection(version_num=3, algorithm='bin',
+                               version_type=VersionType["Untyped"], strict=False,
+                            selected_config=0)
+    m.extract()
 
 
 if __name__ == '__main__':
-    extract_all()
+    extract_all(sys.argv[1])
