cdf17d31b32c4c9e4f544da961a166afb8465ed8
amir9979 <amir9979@gmail.com>
2021-01-21 14:26:20 +0200

    Xmx4096m

--- metrics/version_metrics.py
+++ metrics/version_metrics.py
@@ -440,7 +440,7 @@ class CK(Extractor):
     @staticmethod
     def _execute_command(ck_runner, local_path, out_dir):
         project_path = os.path.join(os.getcwd(), local_path)
-        command = ["java", "-jar", ck_runner, project_path, "True"]
+        command = ["java", '-Xmx4096m', "-jar", ck_runner, project_path, "True"]
         Popen(command, cwd=out_dir).communicate()
         return out_dir
 
@@ -478,7 +478,7 @@ class Mood(Extractor):
 
     @staticmethod
     def _execute_command(mood_runner, local_path, out_dir):
-        command = ["java", "-jar", mood_runner, local_path, out_dir]
+        command = ["java", '-Xmx4096m', "-jar", mood_runner, local_path, out_dir]
         Popen(command).communicate()
 
     def _process_metrics(self):
@@ -523,7 +523,7 @@ class Jasome(Extractor):
 
     @staticmethod
     def _execute_command(jasome_runner, local_path, out_path_to_xml):
-        command = ["java", "-cp", jasome_runner, "org.jasome.executive.CommandLineExecutive", '-xt', local_path, '-o', out_path_to_xml]
+        command = ["java", '-Xmx4096m', "-cp", jasome_runner, "org.jasome.executive.CommandLineExecutive", '-xt', local_path, '-o', out_path_to_xml]
         print('jasome command', command)
         Popen(command).communicate()
 
