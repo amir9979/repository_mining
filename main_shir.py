dea750203f44060e5bdd3a2160b216aa398068c9
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-03-19 10:43:13 +0200

    Update main_shir.py

--- main_shir.py
+++ main_shir.py
@@ -82,8 +82,8 @@ class GetCommit:
 
 if __name__ == '__main__':
 
-    commits_start = int(sys.argv[1]) * 10
-    commits_end = commits_start + 10
+    commits_start = int(sys.argv[1]) * 500
+    commits_end = commits_start + 500
     obj_git = GetCommit('my-tools', commits_start, commits_end)
     write_commit(obj_git.list_of_commit)
     version_before = 'shir_' + str(commits_start)
