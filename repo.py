c2dbd924ea9aa57a4bf09bb37254cbbcb9db87ee
amir9979 <amir9979@gmail.com>
2021-04-06 18:14:40 +0300

    Update repo.py

--- repo.py
+++ repo.py
@@ -7,7 +7,7 @@ from config import Config
 
 class Repo(object):
 
-    def __init__(self, project, commit_to_checkout=None):
+    def __init__(self, project):
         self.project = project
         if not hasattr(project, 'github_user'):
             github_user_name = "apache"
@@ -15,8 +15,6 @@ class Repo(object):
             github_user_name = self.project.github_user
         github_path = os.path.join(Config().config['REPO']['GithubPath'], github_user_name)
         self.clone_if_needed(github_path)
-        # if commit_to_checkout:
-        #     git.Repo(self.project.path).git.checkout(commit_to_checkout.replace('\\', '/'), force=True)
 
     def clone_if_needed(self, github_path):
         if not os.path.exists(self.project.path):
